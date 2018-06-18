b46f872e691bfea3aba0da74efc94bf261bdc5e5
Bartosz Banachowski <bartoszbanachowski@gmail.com>
2018-06-18 06:53:38 +0200

    Added DetailsServiceTest

--- backend/src/test/java/pl/mrs/movie_raiting_system/service/ServiceTest.java
+++ backend/src/test/java/pl/mrs/movie_raiting_system/service/ServiceTest.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import pl.mrs.movie_raiting_system.dao.UserRepository;
 import pl.mrs.movie_raiting_system.dto.TitleInfo;
 import pl.mrs.movie_raiting_system.dto.UserInfo;
+import pl.mrs.movie_raiting_system.dto.theMovieDbApi.MovieDetails;
 import pl.mrs.movie_raiting_system.entities.User;
 
 import java.util.ArrayList;
@@ -19,12 +20,16 @@ public class ServiceTest
     @MockBean
     private TitlesService titlesService;
 
+    @MockBean
+    private DetailsService detailsService;
+
     boolean initialized;
 
     protected UserInfo user1;
     protected User mainUser;
     protected TitleInfo title1, title2;
     protected List<TitleInfo> titles;
+    protected MovieDetails movie1;
 
     protected String query = "Avengers";
 
@@ -63,11 +68,21 @@ public class ServiceTest
                     add(title2);
                 }
             };
+
+            movie1 = MovieDetails.builder()
+                    .title("Thor")
+                    .id(5)
+                    .vote_average(8.9)
+                    .build();
+
             Mockito.when(userRepository.findById(Long.valueOf(user1.getId())))
                     .thenReturn(Optional.of(mainUser));
 
             Mockito.when(titlesService.searchForTitles(query))
                     .thenReturn(titles);
+
+            Mockito.when(detailsService.getMovieInfo(Long.valueOf(movie1.getId())))
+                    .thenReturn(movie1);
             initialized = true;
         }
     }
